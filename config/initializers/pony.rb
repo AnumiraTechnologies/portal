3bcf426439ef3367966946ed3e2a3ef5f7bed1c7
Vivek Koranampatty Surendran <vivek@anumira.com>
2012-12-29 15:30:00 +0530

    'portal

--- config/initializers/pony.rb
+++ config/initializers/pony.rb
@@ -1,12 +1,12 @@
 Pony.options = {
-  :to => 'info@anumira.com',
+  :to => ENV['CONMAILUN'],
   :via => :smtp,
   :via_options => {
     :address              => 'smtp.gmail.com',
     :port                 => '587',
     :enable_starttls_auto => true,
-    :user_name            => 'contact@iryaa.com',
-    :password             => 'REACH.out1349',
+    :user_name            => ENV['CONMAILUN'],
+    :password             => ENV['CONMAILPW'],
     :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
     :domain               => "anumira.com" # the HELO domain provided by the client to the server
   }
